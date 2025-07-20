0dfe2a8373c4fd478ae799e4c8f3bda81a65a16a
Daniel Villegas <daniel.villegas@me.com>
2025-07-20 19:14:23 -0400

    Add DHT11 sensor scripts

---
+++ manglar-tests/build/DHT.d
@@ -0,0 +1,20 @@
+/root/Bela/projects/manglar-tests/build/DHT.o: \
+  /root/Bela/projects/manglar-tests/DHT.cpp \
+  /root/Bela/projects/manglar-tests/DHT.h include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  /usr/xenomai/include/cobalt/time.h include/Utilities.h
+
+/root/Bela/projects/manglar-tests/DHT.h:
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+/usr/xenomai/include/cobalt/time.h:
+
+include/Utilities.h:
---
+++ manglar-tests/build/render.d
@@ -0,0 +1,79 @@
+/root/Bela/projects/manglar-tests/build/render.o: \
+  /root/Bela/projects/manglar-tests/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  /usr/xenomai/include/cobalt/time.h include/Utilities.h \
+  libraries/libpd/libpd.h include/DigitalChannelManager.h \
+  libraries/Midi/Midi.h include/AuxTaskNonRT.h include/SchedulableTask.h \
+  include/../include/RtThread.h /usr/xenomai/include/cobalt/pthread.h \
+  include/RtLock.h include/RtMsgFifo.h \
+  /usr/xenomai/include/cobalt/mqueue.h libraries/Scope/Scope.h \
+  libraries/Fft/Fft.h libraries/math_neon/math_neon.h \
+  libraries/Pipe/Pipe.h libraries/Trill/Trill.h include/I2c.h \
+  /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/sys/ioctl.h libraries/Gui/Gui.h \
+  include/JSON.h include/JSONValue.h include/DataBuffer.h \
+  libraries/WSServer/WSServer.h libraries/Serial/Serial.h \
+  /root/Bela/projects/manglar-tests/DHT.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+/usr/xenomai/include/cobalt/time.h:
+
+include/Utilities.h:
+
+libraries/libpd/libpd.h:
+
+include/DigitalChannelManager.h:
+
+libraries/Midi/Midi.h:
+
+include/AuxTaskNonRT.h:
+
+include/SchedulableTask.h:
+
+include/../include/RtThread.h:
+
+/usr/xenomai/include/cobalt/pthread.h:
+
+include/RtLock.h:
+
+include/RtMsgFifo.h:
+
+/usr/xenomai/include/cobalt/mqueue.h:
+
+libraries/Scope/Scope.h:
+
+libraries/Fft/Fft.h:
+
+libraries/math_neon/math_neon.h:
+
+libraries/Pipe/Pipe.h:
+
+libraries/Trill/Trill.h:
+
+include/I2c.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/sys/ioctl.h:
+
+libraries/Gui/Gui.h:
+
+include/JSON.h:
+
+include/JSONValue.h:
+
+include/DataBuffer.h:
+
+libraries/WSServer/WSServer.h:
+
+libraries/Serial/Serial.h:
+
+/root/Bela/projects/manglar-tests/DHT.h:
